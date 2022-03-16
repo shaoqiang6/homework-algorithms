cd74fa5557cb6270fb66837a22a3c6f2a5fa46f2
yanshaoqiang <shaoqiang_yan@163.com>
2022-03-16 22:05:27 +0800

    treeNode opt

--- common/utils/TreeNode.java
+++ common/utils/TreeNode.java
@@ -11,4 +11,21 @@ public class TreeNode {
         this.left = left;
         this.right = right;
     }
+
+    @Override
+    public String toString() {
+        return "TreeNode{" +
+                "val=" + val +
+                ", left=" + (left == null ? "null" : left.val) +
+                ", right=" + (right== null ? "null" : right.val) +
+                '}';
+    }
+
+    public static TreeNode mock() {
+        int val = 1;
+        TreeNode root = new TreeNode(++val);
+        root.left = new TreeNode(++val);
+        root.right = new TreeNode(++val);
+        return root;
+    }
 }
